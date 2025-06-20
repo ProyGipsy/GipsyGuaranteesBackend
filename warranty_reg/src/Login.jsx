60db06ad9944716e3609b804a4b28fabf2176710
Morax19 <danielhdez19@gmail.com>
2025-06-19 21:49:10 -0400

    .

--- warranty_reg/src/Login.jsx
+++ warranty_reg/src/Login.jsx
@@ -8,7 +8,7 @@ function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://localhost:3000/login', {
+      const response = await fetch('http://localhost:5000/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
