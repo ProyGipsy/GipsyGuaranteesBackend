6ecc5ea443d970a5ffe01fb867eb63a2cba18ed8
Morax19 <danielhdez19@gmail.com>
2025-06-25 15:44:16 -0400

    Another try to fix CORS in the app for front and back communication

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, request, jsonify
+import os
+import json
 from flask_cors import CORS
 from werkzeug.security import generate_password_hash
-import json
-import os
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:5000"])
+CORS(app, origins=["http://localhost:5173", "http://127.0.0.1:5173"])
 
 @app.route('/')
 def home():
