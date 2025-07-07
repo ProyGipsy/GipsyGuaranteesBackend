caedb1f85876b1d5241d40b2aeb28612f9ce7a16
Morax19 <danielhdez19@gmail.com>
2025-07-07 12:32:17 -0400

    ...

---
+++ warranty/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import submit_registration, register_warranty, change_password, forgot_password
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('submitRegistration/', submit_registration, name='submit_registration'),
+    path('registerWarranty/', register_warranty, name='register_warranty'),
+    path('changePassword/', change_password, name='change_password'),
+    path('forgotPassword/', forgot_password, name='forgot_password'),
+]
